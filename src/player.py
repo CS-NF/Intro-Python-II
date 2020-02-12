f61c724ec96f72759ae4ab22b70389ef63cd171e
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-02-12 17:33:04 -0500

    added to Room class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, p_current_room, p_name):
+        self.p_current_room = p_current_room
+        self.p_name = p_name
+
+
--- src/room.py
+++ src/room.py
@@ -1,9 +1,25 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
-    def __init__(self, outside, foyer, overlook, narrow, treasure):
-        self.outside = outside
-        self.foyer = foyer
-        self.overlook = overlook
-        self.narrow = narrow
-        self.treasure = treasure
+    def __init__(self, room_name, description):
+        self.room_name = room_name
+        self.description = description
+        self.n_to = None
+        self.s_to = None 
+        self.e_to = None 
+        self.w_to = None 
+
+    def __str__(self):
+        print(f"{self.room_name} is {self.description}")
+
+    def room_direction(self, direction):
+        if direction == "n":
+            return self.n_to
+        elif direction == "s":
+            return self.s_to:
+        elif direction == "e":
+            return self.e_to
+        elif direction == "w":
+            return self.w_to
+        else:
+            return None
